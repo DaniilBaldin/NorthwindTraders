7188755df2e844d2e8fd22c3fbe189f73493b629
daniil <daniil_baldin@ukr.net>
2022-11-21 16:12:50 +0200

    Fix

--- src/controllers/Dashboard/dashboardController.ts
+++ src/controllers/Dashboard/dashboardController.ts
@@ -18,11 +18,53 @@ const sendAPIRequest = async (ipAddress: any) => {
 };
 
 const dashboardController: RequestHandler = async (req, res) => {
-    const ip = req.socket.remoteAddress;
+    const IP_HEADERS = [
+        'Forwarded',
+        'Forwarded-For',
+        'X-Forwarded',
+        'X-Forwarded-For', // may contain multiple IP addresses in the format: 'client IP, proxy 1 IP, proxy 2 IP' - we use first one
+        'X-Client-IP',
+        'X-Real-IP', // Nginx proxy, FastCGI
+        'X-Cluster-Client-IP', // Rackspace LB, Riverbed Stingray
+        'Proxy-Client-IP',
+        'CF-Connecting-IP', // Cloudflare
+        'Fastly-Client-Ip', // Fastly CDN and Firebase hosting header when forwared to a cloud function
+        'True-Client-Ip', // Akamai and Cloudflare
+        'WL-Proxy-Client-IP',
+        'HTTP_X_FORWARDED_FOR',
+        'HTTP_X_FORWARDED',
+        'HTTP_X_CLUSTER_CLIENT_IP',
+        'HTTP_CLIENT_IP',
+        'HTTP_FORWARDED_FOR',
+        'HTTP_FORWARDED',
+        'HTTP_VIA',
+        'REMOTE_ADDR',
+
+        // you can add more matching headers here ...
+    ];
+
+    const getRequestIpAddress = (req: any) => {
+        const headers = req.headers;
+        for (const header of IP_HEADERS) {
+            const value = headers[header];
+            if (value) {
+                const parts = value.split(/\s*,\s*/g);
+                return parts[0] ?? null;
+            }
+        }
+        const client = req.connection ?? req.socket ?? req.info;
+        if (client) {
+            return client.remoteAddress ?? null;
+        }
+        return null;
+    };
+    const ip = getRequestIpAddress(req);
+    console.log(ip);
+    // const ip = req.headers.host;
     // console.log(ip);
     // const ipAddress = IP.address('public');
-    const ipAddressInformation = await sendAPIRequest(ip);
-    console.log(ipAddressInformation);
+    // const ipAddressInformation = await sendAPIRequest(ip);
+    // console.log(ipAddressInformation);
 
     // const options = {
     //     method: 'GET',
