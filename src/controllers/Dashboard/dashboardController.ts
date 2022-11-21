6693815ec0693216e1ff91938bd6ea71832fbb10
daniil <daniil_baldin@ukr.net>
2022-11-21 15:58:21 +0200

    Fix

--- src/controllers/Dashboard/dashboardController.ts
+++ src/controllers/Dashboard/dashboardController.ts
@@ -6,21 +6,24 @@ import path from 'path';
 dotenv.config({
     path: path.join(__dirname, '../', '/.env'),
 });
+import IP from 'ip';
+const API_KEY = '993fbdff21894ce68d4098491b66e5f7';
+const URL = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + API_KEY;
 
 import logs from '../../models/logs';
 
+const sendAPIRequest = async (ipAddress: any) => {
+    const apiResponse = await axios.get(URL + '&ip_address=' + ipAddress);
+    return apiResponse.data;
+};
+
 const dashboardController: RequestHandler = async (req, res) => {
-    const ip = req.socket.remoteAddress;
-    console.log(ip);
-    const geoData = await axios
-        .get('https://ipgeolocation.abstractapi.com/v1/?api_key=993fbdff21894ce68d4098491b66e5f7')
-        .then((response) => {
-            console.log(response.data);
-            return response.data;
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    // const ip = req.socket.remoteAddress;
+    // console.log(ip);
+    const ipAddress = IP.address();
+    const ipAddressInformation = await sendAPIRequest(ipAddress);
+    console.log(ipAddressInformation);
+
     // const options = {
     //     method: 'GET',
     //     url: ip,
@@ -52,7 +55,7 @@ const dashboardController: RequestHandler = async (req, res) => {
                 }
             });
             res.status(200).json({
-                geoData: geoData,
+                // geoData: geoData,
                 data: {
                     select: select,
                     select_where: select_where,
